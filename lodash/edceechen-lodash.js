260277a6de4f074facf55c3d45b758ef5ec40512
miao <miao@qq.com>
2022-01-11 22:20:03 +0800

    dsda

---
+++ lodash/edceechen-lodash.js
@@ -0,0 +1,18 @@
+var Edceechen = {
+  chunk: function (array, size) {
+    var l = array.length
+    var list = []
+    var newarray = []
+    var k = 0
+    for (var i = 0; i < l; i++) {
+      list.push(array[i])
+      k++
+      if (k == size) {
+        newarray.push(list)
+        list = []
+        k = 0
+      }
+    } newarray.push(list)
+    console.log(newarray)
+  }
+}
